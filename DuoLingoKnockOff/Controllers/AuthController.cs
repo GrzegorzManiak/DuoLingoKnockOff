ab420cc5517ea15346fbee1dbb2f99f3ee3cc32f
Grzegorz Maniak <git@grzegorz.ie>
2025-04-13 18:40:40 +0100

    Feat: Implemented AuthController

--- DuoLingoKnockOff/Controllers/AuthController.cs
+++ DuoLingoKnockOff/Controllers/AuthController.cs
@@ -1,5 +1,9 @@
+using System.Security.Cryptography;
+using System.Text;
+using DuoLingoKnockOff.Data.Entities;
 using DuoLingoKnockOff.Data.Repos.Interfaces;
 using DuoLingoKnockOff.DTO;
+using DuoLingoKnockOff.Helpers;
 using DuoLingoKnockOff.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,12 +16,52 @@ public class AuthController(IUserRepository userRepository, ITokenService tokenS
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
+        var (usernameCased, usernameUncased) = UsernameHelper.GetUsernameVariants(registerDto.Username);
+        if (await userRepository.GetUserByUsernameAsync(usernameUncased) != null)
+            return BadRequest("Username is taken");
 
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(registerDto.Password);
+        var user = new User
+        {
+            UsernameCased = usernameCased,
+            UsernameUncased = usernameUncased,
+            Email = registerDto.Email,
+            PasswordHash = hashedPassword,
+            PreferredLanguage = registerDto.PreferredLanguage,
+            DateJoined = DateTime.UtcNow,
+            LastActive = DateTime.UtcNow
+        };
+        
+        await userRepository.AddAsync(user);
+        await userRepository.SaveAllAsync();
+        
+        return new UserDto
+        {
+            Id = user.Id,
+            Username = user.UsernameCased,
+            Token = tokenService.CreateToken(user),
+            PreferredLanguage = user.PreferredLanguage
+        };
     }
     
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-
+        var user = await userRepository.GetUserByUsernameAsync(loginDto.Username);
+        if (user == null) return Unauthorized("Invalid username");
+        
+        if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash))
+            return Unauthorized("Invalid password");
+        
+        user.LastActive = DateTime.UtcNow;
+        await userRepository.SaveAllAsync();
+        
+        return new UserDto
+        {
+            Id = user.Id,
+            Username = user.UsernameCased,
+            Token = tokenService.CreateToken(user),
+            PreferredLanguage = user.PreferredLanguage
+        };
     }
 }
