f8c5d9cbc9468c5f80746187efecac6c74fc25ec
Grzegorz Maniak <git@grzegorz.ie>
2025-04-13 21:58:58 +0100

    Feat: Added ChallengeScorePenalty property to AppConfig

--- DuoLingoKnockOff/Data/Context/AppConfig.cs
+++ DuoLingoKnockOff/Data/Context/AppConfig.cs
@@ -4,4 +4,5 @@ public class AppConfig
 {
     public int TokenValidDays { get; set; } = 7;
     public int TotalLeaderboardEntries { get; set; } = 30;
+    public int ChallengeScorePenalty { get; set; } = 20;
 }
